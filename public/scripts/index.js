8cc54327cd7eeec56ccc8d7c4c175e6e4f5da7e5
bara <s12010060@stu.najeh.edu>
2023-12-01 19:36:52 +0200

    finished reading

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,11 @@
 let trainingData = [];
 
 function toggleDataInput() {
-    const dataChoice = document.getElementById('data-choice');
-    const dynamicData = document.getElementById('dynamic-data');
-    const fileData = document.getElementById('file-data');
+    const dataChoice = document.getElementById('data-input-choice');
+    const dynamicData = document.getElementById('manual-entry');
+    const fileData = document.getElementById('file-entry');
 
-    if (dataChoice.value === 'dynamic') {
+    if (dataChoice.value === 'manual') {
         dynamicData.style.display = 'block';
         fileData.style.display = 'none';
     } else {
@@ -14,38 +14,13 @@ function toggleDataInput() {
     }
 }
 
-
-// Update the enterData function in your main.js file
-
-function enterData() {
-    const sweetness = parseFloat(document.getElementById('sweetness').value);
-    const color = document.getElementById('color').value;
-
-    if (isNaN(sweetness) || sweetness < 1 || sweetness > 10 || color.trim() === '') {
-        alert('Please enter valid data.');
-        return;
-    }
-
-    const fruitType = predictFruitType(sweetness, color); // Implement your prediction logic here
-
-    // Create an image element for the entered data
-    const img = document.createElement('img');
-    img.src = `path/to/${fruitType}.jpg`; // Replace with the actual path to your images
-    img.alt = fruitType;
-
-    // Append the image to the input images container
-    const inputImagesContainer = document.getElementById('input-images');
-    inputImagesContainer.innerHTML = '';
-    inputImagesContainer.appendChild(img);
-}
-
 // Example function to predict the fruit type (replace with your actual prediction logic)
 function predictFruitType(sweetness, color) {
     if (color === 'red' && sweetness !== 0) {
         return 'apple';
     } else if (color === 'yellow') {
         return 'banana';
-    } else if (color === 'purple') {
+    } else if (color === 'darkBlue') {
         return 'grape';
     } else if (color === 'orange'){
         return 'orange';
@@ -53,20 +28,94 @@ function predictFruitType(sweetness, color) {
 }
 
 
-// ... previous neural network and training functions ...
+function trainNeuralNetwork() {
+    const dataChoice = document.getElementById('data-input-choice');
+    const learningRate = parseFloat(document.getElementById('learning-rate').value);
+    const maxEpochs = parseInt(document.getElementById('max-epochs').value);
+    const numberOfNeurons = parseInt(document.getElementById('neorons').value);
 
-function testModel() {
-    // Get values from test model fields
-    const testSweetness = parseFloat(document.getElementById('test-sweetness').value);
-    const testColor = document.getElementById('test-color').value;
-
-    // Validate inputs
-    if (isNaN(testSweetness) || testSweetness < 1 || testSweetness > 10 || testColor.trim() === '') {
-        alert('Please enter valid test data.');
+    if (isNaN(learningRate) || isNaN(maxEpochs) || isNaN(numberOfNeurons)) {
+        alert('Please enter valid training configuration.');
         return;
     }
 
-    // Implement testing logic with the entered test data
-    console.log('Testing model with:', { testSweetness, testColor });
-    // Optionally, you can display the test results on the webpage.
+    if (dataChoice.value === 'file') {
+        const fileInput = document.getElementById('data-file');
+        const file = fileInput.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                const fileContent = e.target.result;
+
+                // Split the file content into lines
+                const lines = fileContent.split('\n');
+
+                console.log('Number of lines:', lines.length);
+
+                // Process each line
+                lines.forEach((line, index) => {
+                    console.log(`Processing line ${index + 1}: ${line}`);
+
+                    // Split the line into data values (assuming space-separated values)
+                    const values = line.trim().split(/\s+/);
+
+                    console.log('Values:', values);
+
+                    // Check if expected values are present
+                    if (values.length >= 3) {
+                        const sweetnessData = parseFloat(values[0]);
+                        const colorData = values[1].trim();
+                        const expectedOutputData = values[2].trim();
+
+                        console.log('sweetnessData:', sweetnessData);
+                        console.log('colorData:', colorData);
+                        console.log('expectedOutputData:', expectedOutputData);
+
+                        if (!isNaN(sweetnessData) && !isNaN(expectedOutputData)) {
+                            // Create an object for each line
+                            const inputData = {
+                                sweetness: sweetnessData,
+                                color: colorData,
+                                expectedOutput: expectedOutputData,
+                                learningRate: learningRate,
+                                maxEpochs: maxEpochs,
+                                numberOfNeurons: numberOfNeurons
+                                // Add more properties as needed
+                            };
+
+                            // Push the data to the trainingData array
+                            trainingData.push(inputData);
+
+                            // Print the input data to the console (adjust this part based on your needs)
+                            console.log('Training Data:', inputData);
+                        }
+                    }
+                });
+            };
+
+            reader.readAsText(file);
+        }
+    }else {
+        const sweetnessData = parseFloat(document.getElementById('sweetness').value);
+        const colorData = document.getElementById('color').value;
+        const expectedOutputData = parseFloat(document.getElementById('expected').value);
+
+        const inputData = {
+            sweetness: isNaN(sweetnessData) ? 0 : sweetnessData,
+            color: colorData,
+            expectedOutput: isNaN(expectedOutputData) ? 0 : expectedOutputData,
+            learningRate: learningRate,
+            maxEpochs: maxEpochs,
+            numberOfNeurons: numberOfNeurons
+            // Add more properties as needed
+        };
+
+        // Push the data to the trainingData array
+        trainingData.push(inputData);
+
+        // Print the input data to the console (adjust this part based on your needs)
+        console.log('Training Data:', inputData);
+    }
 }
